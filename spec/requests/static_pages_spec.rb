8f68464b9f6d923bf2c987b30e9dc15503588735
vagrant <vagrant@packer-debian-8.1-amd64>
2015-09-25 21:33:41 +0000

    Add base_title variable to spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Micropost" }
+  
   describe "Home pages" do
     it "should have the content 'Micropost'" do
      visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Micropost | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -21,7 +24,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Micropost | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -33,7 +36,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Micropost | About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -45,7 +48,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Micropost | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
